fdbd6ec3862cc3c13ca50967a49438789da47336
Fiorcane <10619082@polimi.it>
2020-03-21 10:22:32 +0100

    First commit

---
+++ src/main/java/it/polimi/ingsw/Model/Player.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.Model;
+
+public class Player {
+    private final String nickname;
+
+    private
+
+
+
+    public Player(String nickname) {
+        this.nickname = nickname;
+    }
+
+}
---
+++ src/main/java/it/polimi/ingsw/Model/Space.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.Model;
+
+public class Space {
+    private final int x;
+    private final int y;
+    private int height;
+    private Worker worker;
+
+
+    public Space(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.height = 0;
+        this.worker = null;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Worker getWorker() {
+        return worker;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWorker(Worker worker) {
+        this.worker = worker;
+    }
+
+
+}
