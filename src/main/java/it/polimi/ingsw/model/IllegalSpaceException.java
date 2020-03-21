082d7fa38de33cbd40a1356b5f7fbac31c8e8d59
Fiorcane <10619082@polimi.it>
2020-03-21 17:32:23 +0100

    Codice con move e build (model)

---
+++ src/main/java/it/polimi/ingsw/model/IllegalSpaceException.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.model;
+
+public class IllegalSpaceException extends Exception{
+    public IllegalSpaceException() {
+        super();
+    }
+
+    public IllegalSpaceException(String message) {
+        super(message);
+    }
+}
