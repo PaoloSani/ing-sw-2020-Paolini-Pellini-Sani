515973a0b23c349ae75b062687ea9d7a60bbbec5
rpellini <riccardo.pellini@mail.polimi.it>
2020-03-23 21:04:23 +0100

    Aggiunto MoveApollo

--- src/main/java/it/polimi/ingsw/model/MoveApollo.java
+++ src/main/java/it/polimi/ingsw/model/MoveApollo.java
@@ -1,7 +1,53 @@
 package it.polimi.ingsw.model;
 
 public class MoveApollo implements Move {
-    public boolean execute(Worker worker, Space space) throws IllegalSpaceException{
+    public boolean execute(Worker worker, Space nextSpace) throws IllegalSpaceException{
+        int currX,currY,currH;
+        currX = worker.getSpace().getX();
+        currY = worker.getSpace().getY();
+        currH = worker.getSpace().getHeight();
+        boolean result;
 
+        //reset del boolean Athena nella classe costraint
+        if( worker.getPlayer().getGodName() == "Athena" ){
+            worker.getPlayer().getModel().getConstraint().setAthena(false);
+        }
+
+        // controllo il contenuto di nextSpace
+        if ( ( currX - nextSpace.getX() ) > 1 || ( currX - nextSpace.getX() ) < -1    ||     //riga non valida
+                ( currY - nextSpace.getY() ) > 1 || ( currY - nextSpace.getY() ) < -1 ||     //colonna non valida
+                nextSpace.getHeight() - currH <= 1                                    ||     //sale più di un livello
+                currX == nextSpace.getX() && currY == nextSpace.getY()                ||     //la prossima cella è quella corrente
+                (nextSpace.getWorker() != null &&
+                        nextSpace.getWorker().getPlayer().equals(worker.getPlayer())  ||     //la prossima cella è occupata da un worker alleato
+                nextSpace.getHeight() == 4                                            ||     //la prossima cella è una cupola
+
+                //se Athena è true controllo che non si possa salire
+                (worker.getPlayer().getModel().getConstraint().athenaBlocks() && (nextSpace.getHeight() - currH == 1)))
+
+            throw new IllegalSpaceException( "Space not accepted!" );
+
+        //controllo condizione di vittoria prima di aggiornare le celle
+        result = worker.getPlayer().isWinner(worker.getSpace(), nextSpace);
+
+        //se sono atena e sono salito setto il flag atena a true
+        if( worker.getPlayer().getGodName() == "Athena" && ( nextSpace.getHeight() - currH == 1 ) ){
+            worker.getPlayer().getModel().getConstraint().setAthena(true);
+        }
+
+        if( nextSpace.getWorker() == null ) {                     //aggiorno la posizione del worker e le space precedente e corrente nella table
+            worker.getSpace().setWorker(null);                    //setto il worker della space precedente a null (la svuoto)
+            worker.setSpace(nextSpace);                           //setto attributo space del worker con la space successiva
+            worker.getSpace().setWorker(worker);                  //setto attributo worker nella nuova space con il valore del mio worker
+        }
+
+        else{
+            worker.getSpace().setWorker(nextSpace.getWorker());   //SWAP di worker con il worker avversario. Non controllo se è mio
+            nextSpace.getWorker().setSpace(worker.getSpace());    //perché l'ho già controllato nel primo if
+            worker.setSpace(nextSpace);
+            worker.getSpace().setWorker(worker);
+        }
+        
+        return result;
     }
 }
