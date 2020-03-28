b1e58804e6d3ca3443303ff9074e18e4870eb5b7
PaoloSani <61708273+PaoloSani@users.noreply.github.com>
2020-03-28 15:21:25 +0100

    Sistemata cartella UML

---
+++ Test/it/polimi/ingsw/model/BuildAtlasTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuildAtlasTest {
+
+    private Space currSpace, space;
+    private Worker myWorker, otherWorker;
+    private BuildAtlas buildAtlas = new BuildAtlas();
+    private Model model = new Model();
+    private Player player = new Player("test", model);
+    private int level;
+
+
+    @Test ( expected = IllegalSpaceException.class)
+    public void executeTest() throws IllegalSpaceException {
+        currSpace = new Space(4,1);
+        space = new Space(5, 1);
+        myWorker = new Worker(player);
+        myWorker.setSpace(currSpace);
+        level = 1;
+        buildAtlas.execute(myWorker, space, level);
+    }
+}
