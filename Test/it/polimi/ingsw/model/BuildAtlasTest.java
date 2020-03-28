d4d47437bf8a78e9140e3411178b3364bce1aa9c
PaoloSani <61708273+PaoloSani@users.noreply.github.com>
2020-03-28 17:17:09 +0100

    Classi di test

--- Test/it/polimi/ingsw/model/BuildAtlasTest.java
+++ Test/it/polimi/ingsw/model/BuildAtlasTest.java
@@ -16,7 +16,7 @@ public class BuildAtlasTest {
 
     @Test ( expected = IllegalSpaceException.class)
     public void executeTest() throws IllegalSpaceException {
-        currSpace = new Space(1,4);
+        currSpace = new Space(4,1);
         space = new Space(5, 1);
         myWorker = new Worker(player);
         myWorker.setSpace(currSpace);
