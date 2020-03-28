b33faf3ad1026481ae7da3aca062324917ec80fa
rpellini <riccardo.pellini@mail.polimi.it>
2020-03-28 15:35:23 +0100

    test

---
+++ Test/it/polimi/ingsw/model/BuildAtlasTest.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildAtlasTest {
+
+    @Test
+    void execute() {
+        Space currSpace, nextSpace;
+        Worker myWorker, otherWorker;
+        Player player;
+        Model model;
+
+
+
+    }
+}
