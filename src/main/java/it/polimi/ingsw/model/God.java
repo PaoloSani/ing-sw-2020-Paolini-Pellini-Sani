620d20a2ffae6d712ea7f360f766afa56cd69dc6
PaoloSani <61708273+PaoloSani@users.noreply.github.com>
2020-04-04 11:39:27 +0200

    Enum God

---
+++ src/main/java/it/polimi/ingsw/model/God.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model;
+
+public enum God {
+    APOLLO, ARTEMIS, ATHENA, ATLAS, CHARON, DEMETER, HEPHAESTUS, HYPNUS, MINOTAUR, PAN, POSEIDON, PROMETHEUS, TRITON, ZEUS;
+
+    //private String description;
+    //private image
+}
