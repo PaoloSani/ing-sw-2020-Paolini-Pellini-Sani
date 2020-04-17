fc37ec50318f6e9ccb5d5cb7b7379c4df26b39e9
Paolo <paolo.sani98@gmail.com>
2020-04-17 14:37:00 +0200

    Pattern Observer su SettingPlayers e correzione bug

--- src/main/java/it/polimi/ingsw/util/GameState.java
+++ src/main/java/it/polimi/ingsw/util/GameState.java
@@ -3,9 +3,10 @@ package it.polimi.ingsw.util;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.virtualView.PlayersInTheGame;
 
+//TODO: Meglio aggiorarnare il tipo di Observable T in Observer<T>, creiamo una super classe di PlayersIntheGame?
 public interface GameState extends Observer<PlayersInTheGame> {
-    public void changeState(GameState nextState);
-    public void execute();
+    void changeState(GameState nextState);
+    void execute();
 
     @Override
     void update(PlayersInTheGame message);
