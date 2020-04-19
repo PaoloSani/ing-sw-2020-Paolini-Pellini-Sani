621e437388a49c7e7ca024e558ecb75192b79acb
Paolo <paolo.sani98@gmail.com>
2020-04-19 15:48:11 +0200

    Riscrittura BuildState, CharonSwitchState, ChooseWorkerState

--- src/main/java/it/polimi/ingsw/controller/BuildState.java
+++ src/main/java/it/polimi/ingsw/controller/BuildState.java
@@ -25,12 +25,19 @@ public class BuildState implements GameState {
 
     @Override
     public void execute() {
+        //update: riceve una cella in cui costruire
+        //execute: esegue la costruzione
+        //changeState: porta in choosingWorker se non ha poteri speciali
+
+        // prendo le coordinate della cella dove costruire:
+        toBuild = backEnd.getGame().getSpace(backEnd.getGameMessage().getSpace1()[0], backEnd.getGameMessage().getSpace1()[1]);
+        level = backEnd.getGameMessage().getLevel();
+
         if (backEnd.getCurrPlayer().getGod() == God.DEMETER && counterDemeter >= 0){
             lastSpace = toBuild;
             counterDemeter++;
         }
 
-        //TODO: correggere il pattern strategy per BuildHephaestus --> portarlo a BuildDefault
         if (backEnd.getCurrPlayer().getGod() == God.HEPHAESTUS && counterHephaestus == 0){
             lastSpace = toBuild;
             counterHephaestus++;
@@ -41,11 +48,11 @@ public class BuildState implements GameState {
             else counterHephaestus++;
         }
 
-        if ( counterPoseidon >= 1 ){
+        if ( counterPoseidon >= 0 ){
             counterPoseidon++;
         }
 
-        if (!hephaestusConstraint) {
+        if (!hephaestusConstraint && counterPoseidon <= 3) {
             try {
                 backEnd.getCurrPlayer().buildSpace(backEnd.getCurrWorker(), toBuild, level);
             } catch (IllegalSpaceException e) {
@@ -53,27 +60,13 @@ public class BuildState implements GameState {
             }
         }
 
-        if (    ( backEnd.getCurrPlayer().getGod() != God.POSEIDON                               &&
-                  backEnd.getCurrPlayer().getGod() != God.DEMETER                                &&
-                  backEnd.getCurrPlayer().getGod() != God.HEPHAESTUS )                           ||
-                ( backEnd.getCurrPlayer().getGod() == God.POSEIDON && counterPoseidon == 4 )     ||      //Significa che ho costruito già 3 volte
-                ( backEnd.getCurrPlayer().getGod() == God.DEMETER && counterDemeter == 2 )       ||
-                ( backEnd.getCurrPlayer().getGod() == God.HEPHAESTUS && counterHephaestus == 1 )  ) {
-
-            resetBuilding();
-            backEnd.updateCurrPlayer();
-            changeState(backEnd.chooseWorkerState);
-        }
 
         if ( backEnd.getCurrPlayer().getGod() == God.POSEIDON ){
-            if ( counterPoseidon == 0 ){
+            if ( counterPoseidon == -1 ){
                 backEnd.setCurrWorker(backEnd.getCurrPlayer().getOtherWorker(backEnd.getCurrWorker()));        //cambio lavoratore
-                if ( backEnd.getCurrWorker().getSpace().getHeight() != 0 ) {      //se non è a terra il suo potere non vale
-                    resetBuilding();
-                    backEnd.updateCurrPlayer();
-                    changeState(backEnd.chooseWorkerState);
+                if ( backEnd.getCurrWorker().getSpace().getHeight() == 0 ) {      //se non è a terra il suo potere non vale
+                    counterPoseidon++;      //lo aggiorno già a 1 così mi accorgo che ho attivato il suo potere e sto costruendo col secondo worker
                 }
-                counterPoseidon++;      //lo aggiorno già a 1 così mi accorgo che ho attivato il suo potere e sto costruendo col secondo worker
             }
 
         }
@@ -83,20 +76,13 @@ public class BuildState implements GameState {
     }
 
     @Override
-    public void changeState(GameState state) {
-        backEnd.setCurrState( state );
-    }
-
-    public void resetBuilding(){
+    public void reset(){
         counterDemeter = 0;
-        counterPoseidon = 0;
+        counterPoseidon = -1;
         counterHephaestus = 0;
         hephaestusConstraint = false;
         lastSpace = null;
     }
 
-    //update: riceve una cella in cui costruire
-    //execute: esegue la costruzione
-    //changeState: porta in choosingWorker se non ha poteri speciali
 
 }
