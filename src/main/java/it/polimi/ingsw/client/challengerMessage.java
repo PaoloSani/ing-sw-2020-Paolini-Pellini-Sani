13f96536d28910cd4244ca2dec086c5de76ed9bd
Paolo <paolo.sani98@gmail.com>
2020-04-26 12:50:29 +0200

    Inizio scrittura FrontEnd

---
+++ src/main/java/it/polimi/ingsw/client/challengerMessage.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.model.God;
+
+public class challengerMessage {
+    int numOfPlayers;
+    God god1;
+    God god2;
+    God god3;
+
+
+    public int getNumOfPlayers() {
+        return numOfPlayers;
+    }
+
+    public void setNumOfPlayers(int numOfPlayers) {
+        this.numOfPlayers = numOfPlayers;
+    }
+}
