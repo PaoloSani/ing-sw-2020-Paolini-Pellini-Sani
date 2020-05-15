0a360b167e8e2979b17cdfb2e260cf6e515ad570
rpellini <riccardo.pellini@mail.polimi.it>
2020-05-15 15:09:01 +0200

    Introduzione di javaFX per GUI (in GraphicalGame)

--- src/main/java/it/polimi/ingsw/GUI/GraphicalGame.java
+++ src/main/java/it/polimi/ingsw/GUI/GraphicalGame.java
@@ -2,13 +2,20 @@ package it.polimi.ingsw.GUI;
 
 import it.polimi.ingsw.CLI.ColourFont;
 import it.polimi.ingsw.CLI.CommandLineGame;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import javax.swing.*;
 
-public class GraphicalGame extends JFrame {
+public class GraphicalGame extends Application {
 
     public void startGUI(){
         System.out.println(ColourFont.ANSI_CYAN_BACKGROUND + "  Not already implemented version" + ColourFont.ANSI_RESET);
         new CommandLineGame().runCLI();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
