d67790f01d195ff745df80d6b154a378da55130b
rpellini <riccardo.pellini@mail.polimi.it>
2020-05-16 18:01:26 +0200

    debug FrontEnd

--- src/main/java/it/polimi/ingsw/server/ServerConnection.java
+++ src/main/java/it/polimi/ingsw/server/ServerConnection.java
@@ -64,12 +64,12 @@ public class ServerConnection implements Runnable {
     public void run() {
         active = true;
          try {
-             socket.setSoTimeout(6000);
+             //socket.setSoTimeout(6000);
              out = new ObjectOutputStream(socket.getOutputStream());
              out.flush();
              in = new ObjectInputStream(socket.getInputStream());
              send("Welcome, server ready!\n");
-             sendPing();
+             //sendPing();
 
              while (active) {
                  Object message;
@@ -87,7 +87,7 @@ public class ServerConnection implements Runnable {
                      notifyFrontEnd();
                  } else if (message instanceof Message) {
                      if (message.equals(Message.CLOSE)) {
-                         //If the gameID is -1, the player isn't playing a match still. Else, I'll end the match he is playing
+                         //If the gameID is -1, the player isn't playing a match yet. Else, I'll end the match he is playing
                          if (gameID != -1) {
                              server.endGame(gameID, this);
                              active = false;
