54f8ff6c2b91d2b7761d337e8779c33f236cc414
rpellini <riccardo.pellini@mail.polimi.it>
2020-05-24 23:27:38 +0200

    table.fxml aggiornata con bottoni già settati

--- src/main/java/it/polimi/ingsw/GUI/TableWindow.java
+++ src/main/java/it/polimi/ingsw/GUI/TableWindow.java
@@ -1,4 +1,35 @@
 package it.polimi.ingsw.GUI;
 
+import javafx.scene.control.Button;
+
 public class TableWindow {
+    public Button button00;
+    public Button button01;
+    public Button button02;
+    public Button button03;
+    public Button button04;
+    public Button button10;
+    public Button button11;
+    public Button button12;
+    public Button button13;
+    public Button button14;
+    public Button button20;
+    public Button button21;
+    public Button button22;
+    public Button button23;
+    public Button button24;
+    public Button button30;
+    public Button button31;
+    public Button button32;
+    public Button button33;
+    public Button button34;
+    public Button button40;
+    public Button button41;
+    public Button button42;
+    public Button button43;
+    public Button button44;
+
+
+
+
 }
