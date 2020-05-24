8ac96453a5358365e071231d5a2d289d9fac48a5
rpellini <riccardo.pellini@mail.polimi.it>
2020-05-24 23:42:03 +0200

    table.fxml aggiornata con bottoni già settati e image

--- src/main/java/it/polimi/ingsw/GUI/TableWindow.java
+++ src/main/java/it/polimi/ingsw/GUI/TableWindow.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.GUI;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class TableWindow {
     public Button button00;
@@ -28,8 +30,34 @@ public class TableWindow {
     public Button button42;
     public Button button43;
     public Button button44;
+    public ImageView image00;
+    public ImageView image01;
+    public ImageView image02;
+    public ImageView image03;
+    public ImageView image04;
+    public ImageView image10;
+    public ImageView image11;
+    public ImageView image12;
+    public ImageView image13;
+    public ImageView image14;
+    public ImageView image20;
+    public ImageView image21;
+    public ImageView image22;
+    public ImageView image23;
+    public ImageView image24;
+    public ImageView image30;
+    public ImageView image31;
+    public ImageView image32;
+    public ImageView image33;
+    public ImageView image34;
+    public ImageView image40;
+    public ImageView image41;
+    public ImageView image42;
+    public ImageView image43;
+    public ImageView image44;
 
 
+    public void doSomething(ActionEvent actionEvent) {
 
-
+    }
 }
